2e1e49616c8df6e18e48ff6519b6b6a447aa773f
wtfkyyy <alrzkyy11@gmail.com>
2026-03-23 21:59:05 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,22 @@
 from flask import Flask, jsonify
-from api.spotify import get_now_playing  # atau fungsi yg ada di repo
+from api.spotify import get_now_playing, is_configured
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Spotify API is running"
+    return "Spotify API Running"
 
 @app.route("/api/spotify")
 def spotify():
-    data = get_now_playing()
-    return jsonify(data)
+    if not is_configured():
+        return jsonify({"error": "Spotify not configured"})
+
+    try:
+        data = get_now_playing()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 # WAJIB untuk Vercel
 handler = app
